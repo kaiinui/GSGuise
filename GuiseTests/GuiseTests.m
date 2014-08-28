081e954777686f4846e88761db39461b5af2d6fa
Kai INUI <lied.der.optik@gmail.com>
2014-08-29 02:23:57 +0900

    test for +gid;

--- GuiseTests/GuiseTests.m
+++ GuiseTests/GuiseTests.m
@@ -6,6 +6,7 @@
 
 @interface GSGuise ()
 + (int)shard;
++ (unsigned long long int)sequence;
 @end
 
 SpecBegin(GSGuise)
@@ -15,6 +16,30 @@ describe(@"GSGuise", ^{
         int shard = [GSGuise shard];
         it(@"Should return same value as before.", ^{
             expect([GSGuise shard]).to.equal(shard);
+        });
+    });
+    
+    describe(@"+sequence", ^{
+        unsigned long long int sequence = [GSGuise sequence];
+        it(@"Should return random value", ^{
+            expect([GSGuise sequence]).notTo.equal(sequence);
+        });
+    });
+    
+    describe(@"+gid", ^{
+        unsigned long long int gid = [GSGuise gid];
+        it(@"Should not return same value as before", ^{
+            expect([GSGuise gid]).notTo.equal(gid);
+        });
+        
+        it(@"Should be larger than former value", ^{
+            sleep(0.1);
+            expect([GSGuise gid]).to.beGreaterThan(gid);
+        });
+        
+        it(@"mod is always same", ^{
+            int mod = 8;
+            expect([GSGuise gid] % mod).to.equal(gid % mod);
         });
     });
 });
